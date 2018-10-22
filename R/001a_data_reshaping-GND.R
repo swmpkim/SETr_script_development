9670fa9e19e38ce5e015d06611c521dab864e97e
swmpkim <kimberly.cressman@dmr.ms.gov>
2018-10-22 11:33:07 -0500

    write to excel, with formatting

--- R/001a_data_reshaping-GND.R
+++ R/001a_data_reshaping-GND.R
@@ -10,6 +10,7 @@ library(janitor)
 library(here)
 
 
+
 #############################
 # Deal with main data frame
 #############################
@@ -32,11 +33,42 @@ dat <- dat %>%
     select(reserve, set_id, arm_position, everything(), -SET, -arm) %>%
     arrange(set_id, date, arm_position)
 
+unique(dat$set_id)
+
+# change set names
+
+# panne to juro_high
+dat$set_id <- gsub('PANNE', 'JURO_High', dat$set_id)
+
+# juro_1,2,3 to juro_low
+dat$set_id <- gsub('JURO-1', 'JURO_Low-1', dat$set_id)
+dat$set_id <- gsub('JURO-2', 'JURO_Low-2', dat$set_id)
+dat$set_id <- gsub('JURO-3', 'JURO_Low-3', dat$set_id)
+
+# juro 4,5,6 to juro_mid
+dat$set_id <- gsub('JURO-4', 'JURO_Mid-1', dat$set_id)
+dat$set_id <- gsub('JURO-5', 'JURO_Mid-2', dat$set_id)
+dat$set_id <- gsub('JURO-6', 'JURO_Mid-3', dat$set_id)
+
+# spal to spalt
+dat$set_id <- gsub('SPAL', 'SPALT', dat$set_id)
+
+unique(dat$set_id)
+
 unique_sets <- unique(dat$set_id)
 
 for(i in seq_along(unique_sets)) {
     dat_sub <- dat[dat$set_id == unique_sets[i], ]
-    filename <-  paste0(tolower(unique_sets[i]), '.csv')
-    filepath <- here('data', 'split_by_site', filename)
-    write_csv(dat_sub, filepath)
+    csvname <-  paste0(tolower(unique_sets[i]), '.csv')
+    csvpath <- here('data', 'split_by_site', csvname)
+    xlname <-  paste0(tolower(unique_sets[i]), '.xlsx')
+    xlpath <- here('data', 'split_by_site', xlname)
+    # write_csv(dat_sub, filepath)
+    wb <- createWorkbook()
+    addWorksheet(wb, 'data')
+    writeDataTable(wb, 1, dat_sub, startRow = 1, startCol = 1,
+              tableStyle = 'TableStyleLight9',
+              bandedRows = TRUE,
+              bandedCols = TRUE)
+    saveWorkbook(wb, xlpath, overwrite = TRUE)
 }
