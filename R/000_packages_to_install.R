71f1c9f88572edfff40b595892d3deebba645c8d
swmpkim <kimberly.cressman@dmr.ms.gov>
2019-01-10 13:43:50 -0600

    updates to install script and gitignore

--- R/000_packages_to_install.R
+++ R/000_packages_to_install.R
@@ -1,22 +1,30 @@
-if(!require(tidyverse)){
-    install.packages('tidyverse')
-}
+# It's possible to write a shorter command to install all these packages
+# But there's always at least one that causes trouble
+# So I like to install one at a time - easier to find the troublemakers
+# Run one line at a time below, and when you get the 'success' message,
+# move on to the next line
 
-# if there are problems installing the whole tidyverse, can just install individual packages:
-# readr
-# ggplot2
-# dplyr
-# tidyr
+install.packages("dplyr")
 
-if(!require(lubridate)){
-    install.packages('lubridate')
-}
+install.packages("tidyr")
 
-if(!require(here)){
-    install.packages('here')
-}
+install.packages("ggplot2")
 
+install.packages("readr")
 
+install.packages("here")
 
+install.packages("lubridate")
 
+install.packages("rmarkdown")
+
+install.packages("shiny")
+
+install.packages("broom")
+
+install.packages("plotly")
+
+install.packages("")
+install.packages("")
+install.packages("")
 
