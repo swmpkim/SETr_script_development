a6f99f908eada9fb8ac9f847a24868576babd896
swmpkim <kimberly.cressman@dmr.ms.gov>
2019-01-14 10:59:37 -0600

    back to one line per package for troubleshooting purposes

--- R/000_packages_to_install.R
+++ R/000_packages_to_install.R
@@ -1,28 +1,40 @@
-# Run one chunk at a time below, and when you get the 'success' message,
-# move on to the next line
+# Run one line at a time below, and when you get the 'success' message,
+# move on to the next line.
 # sucess looks something like this:
 # package ‘here’ successfully unpacked and MD5 sums checked
 
-# if you have any errors, try that chunk again later. 
+# if you have any errors, try to run it in R, not RStudio. It will ask you to
+# choose a CRAN mirror for the session; choose the one that is geographically
+# closest to you.
+
 # If it still doesn't work, copy and paste the whole chunk (including the code you ran) into an email and send it to Kim
 
 
+install.packages("here", quiet = TRUE)
 
-install.packages(c("broom", "ggplot2", "here", "janitor"), 
-                 dependencies = TRUE, 
-                 quiet = TRUE)
+install.packages("janitor", quiet = TRUE)
 
+install.packages("knitr", quiet = TRUE)
 
-install.packages(c("knitr", "lubridate", "nlme", "plotly"), 
-                 dependencies = TRUE, 
-                 quiet = TRUE)
+install.packages("lubridate", quiet = TRUE)
 
+install.packages("nlme", quiet = TRUE)
 
-install.packages(c("readr", "readxl", "rmarkdown", "shiny"), 
-                 dependencies = TRUE, 
-                 quiet = TRUE)
+install.packages("plotly", quiet = TRUE)
 
+install.packages("readr", quiet = TRUE)
 
-install.packages(c("dplyr", "tidyr"), 
-                 dependencies = TRUE, 
-                 quiet = TRUE)
+install.packages("readxl", quiet = TRUE)
+
+install.packages("rmarkdown", quiet = TRUE)
+
+install.packages("shiny", quiet = TRUE)
+
+install.packages("dplyr", quiet = TRUE)
+
+install.packages("tidyr", quiet = TRUE)
+
+install.packages("ggplot2", quiet = TRUE)
+
+install.packages("broom", quiet = TRUE)
+
